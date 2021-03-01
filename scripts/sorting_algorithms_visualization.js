767529f1fafb2517247f10c92d3a0d7340590b9f
MatthiasSagerer <msagerer1@gmail.com>
2021-03-01 21:18:01 +0100

    Flex boxes for settings, speed slider and TODOs

--- scripts/sorting_algorithms_visualization.js
+++ scripts/sorting_algorithms_visualization.js
@@ -1,1 +1,9 @@
 ﻿console.log('Hello World')
+
+var speedslider = document.getElementById("speedslider");
+var speed = document.getElementById("speed");
+speed.innerHTML = speedslider.value;
+
+speedslider.oninput = function () {
+    speed.innerHTML = this.value;
+}
