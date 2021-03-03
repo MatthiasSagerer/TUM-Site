04c74cca7957ee38a270f293dae2e58b967db3ad
MatthiasSagerer <msagerer1@gmail.com>
2021-03-03 19:41:29 +0100

    displaying first rectangle

--- scripts/sorting_algorithms_visualization.js
+++ scripts/sorting_algorithms_visualization.js
@@ -5,3 +5,10 @@ speed.innerHTML = speedslider.value;
 speedslider.oninput = function () {
     speed.innerHTML = this.value;
 }
+
+
+var canvas = document.getElementById('canv');
+var ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.rect(150, 175, 200, 150);
+ctx.stroke();
