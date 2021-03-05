0ea249f3b98507248d2d18acaf11edd0b16d8bbd
MatthiasSagerer <msagerer1@gmail.com>
2021-03-05 20:06:27 +0100

    making countSlider work

--- scripts/sorting_algorithms_visualization.js
+++ scripts/sorting_algorithms_visualization.js
@@ -7,13 +7,9 @@ var speed = document.getElementById("speed");
 speed.innerHTML = speedslider.value;
 
 var heights = []
-var element_count = 10;
+var element_count = 15;
 var height_diff = 10;
 var width = 10;
-
-for (i = 1; i <= element_count; i++) {
-    heights.push(i);
-}
 
 // Durstenfeld Shuffle Algorithm
 function shuffleArray(array) {
@@ -25,10 +21,31 @@ function shuffleArray(array) {
     }
 }
 
+function initHeightsArray(size) {
+    arr = []
+    for (i = 1; i <= size; i++) {
+        arr.push(i);
+    }
+    return arr
+}
+
 countslider.oninput = function () {
     rect_count.innerHTML = this.value;
-    elemt_count = rect_count;
+    element_count = this.value;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    heights = initHeightsArray(this.value);
+    drawRects(heights, ctx);
+    console.log(element_count);
 }
+
+function drawRects(array, context) {
+    for (i = 0; i < array.length; i++) {
+        context.fillRect(400 + (2 * i - array.length + 0.5) * width, (element_count - array[i]) * height_diff + 30, width, height_diff * array[i]);
+    }
+}
+
+// initialize heigts array
+heights = initHeightsArray(15)
 
 speedslider.oninput = function () {
     speed.innerHTML = this.value;
@@ -37,10 +54,4 @@ speedslider.oninput = function () {
 var canvas = document.getElementById('canv');
 var ctx = canvas.getContext('2d');
 
-for (i = 0; i < heights.length; i++) {
-    ctx.beginPath();
-    ctx.rect(400 + (2 * i - heights.length+ 0.5) * width, (element_count - heights[i]) * height_diff + 30, width, height_diff * heights[i]);
-    ctx.stroke();
-}
-// ctx.rect(150, 175, 200, 150);
-// (700 - heights.length*2*width)/2 + i * 2*width
+drawRects(heights, ctx);
